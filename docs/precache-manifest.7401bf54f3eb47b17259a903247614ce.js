7a9a639f770a5388f36e48d5b1a8a53ecaa19f4d
anniavdc <anva1189@gmail.com>
2019-05-13 00:14:17 +0200

    Add docs folder

---
+++ docs/precache-manifest.7401bf54f3eb47b17259a903247614ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb98c99c52c091ac8ce74f74db77613c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302adb745d058376ca94",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "578b1da94717865eb2e3",
+    "url": "/static/js/2.463489ad.chunk.js"
+  },
+  {
+    "revision": "302adb745d058376ca94",
+    "url": "/static/js/main.48dae54e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
